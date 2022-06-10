bbdad7b7125ffd714ad22db55efc5757a1d97778
Louby <louby.trahan@gmail.com>
2022-06-10 02:38:05 -0500

    Navbar and hamburgr updates - more responsive

---
+++ script.js
@@ -0,0 +1,12 @@
+const hamburger = document.querySelector(".hamburger");
+const navigation = document.querySelector(".navigation");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navigation.classList.toggle("active");
+})
+
+document.querySelectorAll(".main-nav-link").forEach(n => n. addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navigation.classList.remove("active");
+}))
