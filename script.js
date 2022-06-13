79487475fda0a290554fc7ec46646227e39f982f
Louby <louby.trahan@gmail.com>
2022-06-13 00:35:18 -0500

    removed script for scroll to top - unneccessary

--- script.js
+++ script.js
@@ -11,31 +11,3 @@ document.querySelectorAll(".main-nav-link").forEach(n => n. addEventListener("cl
     hamburger.classList.remove("active");
     navigation.classList.remove("active");
 }))
-
-//scroll to top functionality
-.document,addEventListener("scroll", handleScroll);
-
-var scrollToTopBtn = document.querySelector(".scroll-up");
-
-function handleScroll() {
-    var scrollableHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    var GOLDEN_RATIO = 0.5;
-
-    if ((document.documentElement.scrollTop / scrollableHeight ) > GOLDEN_RATIO) {
-        //show button
-        scrollToTopBtn.style.display = "block";
-    } else {
-        //hide button
-        scrollToTopBtn.style.display = "none";
-    }
-}
-
-scrollToTopBtn.addEventListener("click", scrollToTop);
-
-function scrollUp() {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    });
-}
-
